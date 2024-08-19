c52092df8f1cca613a7f87d57028ad5b22fe473b
Jefferson <jepperson22@gmail.com>
2024-08-18 21:54:01 -0500

    Actualización del código del servidor Flask para incluir la nueva ruta /get_contact

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   
+
